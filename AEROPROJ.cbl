d847fdcd041201606f61437cb5b2d49656ea19f4
MichelThangavel <thangavel.michel@hotmail.fr>
2016-04-04 17:16:03 +0200

    Added files via upload

--- AEROPROJ.cbl
+++ AEROPROJ.cbl
@@ -42,6 +42,163 @@ WORKING-STORAGE SECTION.
 01 PILTYP.
 	02 CODPIL PIC 9.
 	02 CODTYP PIC 9.
+	
+create table ADRESSE 
+(
+   ADRESSEID            integer                        not null,
+   NUMERO               integer                        not null,
+   TYPEVOIE             char(20)                       not null,
+   NOMVOIE              char(50)                       not null,
+   CODEPOSTAL           integer                        not null,
+   VILLE                char(20)                       not null,
+   PAYS                 char(20)                       not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_ADRESSE primary key (ADRESSEID)
+);
+
+create table AVIONS 
+(
+   CODAV                integer                        not null,
+   CODTYP               char(2)                        not null,
+   CPTHORAV             integer                        not null,
+   CPTINTER             integer                        not null,
+   INFOS                char(50)                       not null,
+   ETATAV               char(1)                        not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_AVIONS primary key (CODAV)
+);
+
+create table CIVILITE 
+(
+   CIVID                integer                        not null,
+   LIBELLE              char(20)                       not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_CIVILITE primary key (CIVID)
+);
+
+create table CONTROLES 
+(
+   NUMCONTROL           integer                        not null,
+   DATECONTROL          char(10)                       not null,
+   RESCONTROL           integer                        not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_CONTROLES primary key (NUMCONTROL)
+);
+
+create table ETAT_AVION 
+(
+   NUMETATAV            char(1)                        not null,
+   NOMETATAV            char(10)                       not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_ETAT_AVION primary key (NUMETATAV)
+);
+
+create table ETAT_PILOTE_PRESENCE 
+(
+   NUMPRESENCE          char(1)                        not null,
+   NOMPRESENCE          char(10)                       not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_ETAT_PILOTE_PRESENCE primary key (NUMPRESENCE)
+);
+
+create table ETAT_PILOTE_SANTE 
+(
+   NUMSANTE             char(1)                        not null,
+   NOMSANTE             char(10)                       not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_ETAT_PILOTE_SANTE primary key (NUMSANTE)
+);
+
+create table ETAT_PILOTE_SITUATION 
+(
+   NUMSITUATION         char(1)                        not null,
+   NOMSITUATION         char(10)                       not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_ETAT_PILOTE_SITUATION primary key (NUMSITUATION)
+);
+
+create table PILOTAGE 
+(
+   NUMETYPE               integer                        not null,
+   NUMPIL               integer                        not null,
+   DATEPILOTAGE         char(10)                       not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_PILOTES primary key (NUMETYPE)
+);
+
+create table PILOTES 
+(
+   NUMPIL               integer                        not null,
+   NOM                  char(20)                       not null,
+   PRENOM               char(20)                       not null,
+   CIV                  integer                        not null,
+   ADRESSE              char (50)                      not null,
+   NBHVOL               integer                        not null,
+   ETAT_PILOTE          char(3)                        not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_PILOTES primary key (NUMPIL)
+);
+
+create table TYPES 
+(
+   NUMTYP               integer                        not null,
+   DESIGN               char(20)                       not null,
+   TARIF                float(2)                       not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_TYPES primary key (NUMTYP)
+);
+
+create table VISITES 
+(
+   NUMVISIT             integer                        not null,
+   DATEVISIT            char(10)                       not null,
+   RESULTAT             char(1)                        not null,
+   TYPVISIT             char(1)                        not null,
+   CODOBJ               integer                        not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_VISITES primary key (NUMVISIT)
+);
+
+create table VOLS 
+(
+   NUMVOL               integer                        not null,
+   DATEDEB              char(10)                       not null,
+   DATEFIN              char(10)                       not null,
+   CPTDEP               integer                        not null,
+   CPTARR               integer                        not null,
+   DESTIN               char(25)                       not null,
+   ETATVOL              char(1)                        not null,
+   NUMAV                integer                        not null,
+   NUMPIL               integer                        not null,
+   DATCRE               char(8)                        not null,
+   DATMAJ				char(8)						   not null,
+   DATSUP				char(8)						   not null,
+   constraint PK_VOLS primary key (NUMVOL)
+);
 
 PROCEDURE DIVISION.
 PRINCIPAL SECTION.

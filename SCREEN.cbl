87908a9507f612572a86a973ebb8b697b8a37e81
mmjdoub <myriam.majdoub@gmail.com>
2016-04-06 11:26:50 +0200

    Added files via upload

--- SCREEN.cbl
+++ SCREEN.cbl
@@ -4,32 +4,33 @@ DATA DIVISION.
 WORKING-STORAGE SECTION.
 77 ERREURS PIC X(50) VALUE IS " ".
 77 CHOIX PIC X VALUE IS " ".
-77 QUITTER PIC 9 VALUE 1.
 77 I PIC 9 VALUE 0.
 77 NB-ESSAI PIC Z.
 01  WDATE.
-   02  WJOUR PIC 99.
-   02  FILLER PIC X VALUE "/".
-   02  WMOIS PIC 99.
-   02  FILLER PIC X VALUE "/".
    02  WANNEE PIC 99.
+   02  WMOIS PIC 99.
+   02  WJOUR PIC 99.
 LINKAGE SECTION.
 SCREEN SECTION.
   01  SCREEN-ACCUEIL.
       05 LINE 01 COL 01 VALUE "******************".
-      05 LINE 02 COL 01 PIC X(10) FROM WDATE.
-      05 LINE 03 COL 01 VALUE "Application de gestion aeroclub".
-      05 LINE 04 COL 01 VALUE "Fonction en cours : presentation generale".
+      05 LINE 02 COL 01 PIC 99 FROM WJOUR.
+	  05 LINE 02 COL 03 VALUE "/".
+	  05 LINE 02 COL 04 PIC 99 FROM WMOIS.
+	  05 LINE 02 COL 06 VALUE "/".
+	  05 LINE 02 COL 07 PIC 99 FROM WANNEE.
+      05 LINE 03 COL 01 VALUE "Application de gestion Aeroclub".
+      05 LINE 04 COL 01 VALUE "Fonction en cours : Présentation générale".
       05 LINE 05 COL 01 VALUE "******************".
       05 LINE 07 COL 01 VALUE "MENU :".
-      05 LINE 09 COL 01 VALUE "0 : liste des avions à réviser et des pilotes malades".
-      05 LINE 10 COL 01 VALUE "1 : dépôt d'un plan de vol".
-      05 LINE 11 COL 01 VALUE "2 : enregistrement d'un vol".
-      05 LINE 12 COL 01 VALUE "3 : mise-à-jour des pilotes".
-      05 LINE 13 COL 01 VALUE "4 : mise à jour des avions".
-      05 LINE 14 COL 01 VALUE "5 : récapitulatif avions".
-      05 LINE 15 COL 01 VALUE "6 : récapitulatif des pilotes".
-      05 LINE 16 COL 01 VALUE "7 : facturation des vols aux pilotes".
+      05 LINE 09 COL 01 VALUE "0 : Liste des avions à réviser et des pilotes malades".
+      05 LINE 10 COL 01 VALUE "1 : Dépôt d'un plan de vol".
+      05 LINE 11 COL 01 VALUE "2 : Enregistrement d'un vol".
+      05 LINE 12 COL 01 VALUE "3 : Mise à jour des pilotes".
+      05 LINE 13 COL 01 VALUE "4 : Mise à jour des avions".
+      05 LINE 14 COL 01 VALUE "5 : Récapitulatif avions".
+      05 LINE 15 COL 01 VALUE "6 : Récapitulatif des pilotes".
+      05 LINE 16 COL 01 VALUE "7 : Facturation des vols aux pilotes".
       05 LINE 19 COL 01 VALUE "******************".   
       05 LINE 20 COL 01 PIC X(43) FROM ERREURS.
       05 LINE 20 COL 45 PIC Z FROM NB-ESSAI.
@@ -41,7 +42,7 @@ PROCEDURE DIVISION.
 DEBUT.
 
 PERFORM AFFICHE-ACCUEIL.
-PERFORM  MENU.
+PERFORM MENU.
 
 AFFICHE-ACCUEIL.
    ACCEPT WDATE FROM DATE
@@ -50,17 +51,17 @@ AFFICHE-ACCUEIL.
 
 MENU.
    EVALUATE CHOIX
-   WHEN "0"
+    WHEN "0"
        CALL "F0"
-   WHEN "1"
+    WHEN "1"
        CALL "F1"
-   WHEN "2"
+    WHEN "2"
        CALL "F2"
-   WHEN "3"
+    WHEN "3"
        CALL "F3"
-   WHEN "4"
+    WHEN "4"
        CALL "F4"
-   WHEN "5"
+    WHEN "5"
        CALL "F5"
     WHEN "6"
        CALL "F6"
@@ -70,7 +71,7 @@ MENU.
        STOP RUN
     WHEN "q"
        STOP RUN   
-   WHEN OTHER
+    WHEN OTHER
         PERFORM ERREUR
 END-EVALUATE.
 
